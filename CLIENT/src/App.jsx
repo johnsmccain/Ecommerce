f3e4a71fc766b9966de4ff9617da3890c92aa96f
johnsmccain <johnsdanlami@mail.com>
2021-12-26 08:05:21 +0000

    initial commit

---
+++ CLIENT/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  const user = false;
+  return (
+    <div className="container">
+      <h1>Hello Programmers</h1>
+      <p>get ready</p>
+    </div>
+  );
+}
+
+export default App;
