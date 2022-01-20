ec6a2de754ba646170056e65833ad1040a501dcd
johnsmccain <johnsdanlami@mail.com>
2022-01-20 11:50:41 +0100

    fix structure

--- CLIENT/src/index.js
+++ CLIENT/src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./style/style.css";
 import App from "./App";
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
